f787eaaa73fcad093d26fdf9971164c1086e4d95
Groosha <groosha@protonmail.com>
2022-03-19 23:47:10 +0300

    Избавляемся от глобального caches, кастуем event в Message, чтобы линтер не ругался лишний раз

--- bot/middlewares/throttling.py
+++ bot/middlewares/throttling.py
@@ -1,19 +1,19 @@
-from typing import Any, Awaitable, Callable, Dict
+from typing import Any, Awaitable, Callable, Dict, cast
 
 from aiogram import BaseMiddleware
 from aiogram.dispatcher.event.handler import HandlerObject
-from aiogram.types import Update
+from aiogram.types import Update, Message
 from cachetools import TTLCache
 
 from bot.const import THROTTLE_TIME_SPIN, THROTTLE_TIME_OTHER
 
-caches = {
-    "spin": TTLCache(maxsize=10_000, ttl=THROTTLE_TIME_SPIN),
-    "default": TTLCache(maxsize=10_000, ttl=THROTTLE_TIME_OTHER)
-}
-
 
 class ThrottlingMiddleware(BaseMiddleware):
+    caches = {
+        "spin": TTLCache(maxsize=10_000, ttl=THROTTLE_TIME_SPIN),
+        "default": TTLCache(maxsize=10_000, ttl=THROTTLE_TIME_OTHER)
+    }
+
     async def __call__(
             self,
             handler: Callable[[Update, Dict[str, Any]], Awaitable[Any]],
@@ -22,11 +22,10 @@ class ThrottlingMiddleware(BaseMiddleware):
     ) -> Any:
         real_handler: HandlerObject = data.get("handler")
         throttling_key = real_handler.flags.get("throttling_key")
-        if throttling_key is not None and throttling_key in caches:
-            # Поскольку мидлварь только для Message, то тип Update всегда Message.
-            # И проверку if isinstance(event, Message) можно пропустить, хотя PyCharm будет ругаться
-            if event.chat.id in caches[throttling_key]:
+        if throttling_key is not None and throttling_key in self.caches:
+            event = cast(Message, event)  # Обещаем, что event будет иметь тип Message и только его
+            if event.chat.id in self.caches[throttling_key]:
                 return
             else:
-                caches[throttling_key][event.chat.id] = None
+                self.caches[throttling_key][event.chat.id] = None
         return await handler(event, data)
