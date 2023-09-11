fe1155004d5e96ced30866cba04987ca0ce95618
Groosha <groosha@protonmail.com>
2023-09-12 01:43:59 +0800

    Updates field validators

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional
 
-from pydantic import validator, SecretStr, RedisDsn
+from pydantic import field_validator, SecretStr, RedisDsn, FieldValidationInfo
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -13,20 +13,16 @@ class FSMMode(str, Enum):
 class Settings(BaseSettings):
     bot_token: SecretStr
     fsm_mode: FSMMode
-    redis: Optional[RedisDsn]
+    redis: Optional[RedisDsn] = None
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
-    @validator("fsm_mode")
-    def fsm_type_check(cls, v):
-        if v not in ("memory", "redis"):
-            raise ValueError("Incorrect fsm_mode. Must be one of: memory, redis")
-        return v
-
-    @validator("redis")
-    def skip_validating_redis(cls, v, values):
-        if values["fsm_mode"] == "redis" and v is None:
-            raise ValueError("Redis config is missing, though fsm_type is 'redis'")
+    @field_validator("redis", mode="after")
+    @classmethod
+    def skip_validating_redis(cls, v: Optional[RedisDsn], info: FieldValidationInfo):
+        if info.data.get("fsm_mode") == FSMMode.REDIS and v is None:
+            err = 'FSM Mode is set to "Redis", but Redis DNS is missing!'
+            raise ValueError(err)
         return v
 
 
