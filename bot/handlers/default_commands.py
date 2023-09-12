028e1c898dfbc150ce82f4f47d71c00a3280680f
Groosha <groosha@protonmail.com>
2023-09-13 00:48:24 +0800

    Use localized texts for handlers

--- bot/handlers/default_commands.py
+++ bot/handlers/default_commands.py
@@ -1,11 +1,10 @@
-from textwrap import dedent
-
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
+from fluent.runtime import FluentLocalization
 
-from bot.const import START_POINTS
+from bot.config_reader import Settings
 from bot.keyboards import get_spin_keyboard
 
 flags = {"throttling_key": "default"}
@@ -13,40 +12,24 @@ router = Router()
 
 
 @router.message(Command("start"), flags=flags)
-async def cmd_start(message: Message, state: FSMContext):
-    start_text = """\
-    <b>Добро пожаловать в наше виртуальное казино!</b>
-    У вас {points} очков. Каждая попытка стоит 1 очко, а за выигрышные комбинации вы получите:
-    
-    3 одинаковых символа (кроме семёрки) — 7 очков
-    7️⃣7️⃣▫️ — 5 очков (квадрат = что угодно)
-    7️⃣7️⃣7️⃣ — 10 очков
-    
-    <b>Внимание</b>: бот предназначен исключительно для демонстрации, и ваши данные могут быть сброшены в любой момент! 
-    Помните: лудомания — это болезнь, и никаких платных опций в боте нет.
-    
-    Убрать клавиатуру — /stop
-    Показать клавиатуру, если пропала — /spin
-    """
-    await state.update_data(score=START_POINTS)
-    await message.answer(dedent(start_text).format(points=START_POINTS), reply_markup=get_spin_keyboard())
+async def cmd_start(message: Message, state: FSMContext, l10n: FluentLocalization, config: Settings):
+    start_text = l10n.format_value("start-text", {"points": config.starting_points})
+
+    await state.update_data(score=config.starting_points)
+    await message.answer(start_text, reply_markup=get_spin_keyboard(l10n))
 
 
 @router.message(Command("stop"), flags=flags)
-async def cmd_stop(message: Message):
+async def cmd_stop(message: Message, l10n: FluentLocalization):
     await message.answer(
-        "Клавиатура удалена. Начать заново: /start, вернуть клавиатуру и продолжить: /spin",
+        l10n.format_value("stop-text"),
         reply_markup=ReplyKeyboardRemove()
     )
 
 
 @router.message(Command("help"), flags=flags)
-async def cmd_help(message: Message):
-    help_text = \
-        "В казино доступно 4 элемента: BAR, виноград, лимон и цифра семь. Комбинаций, соответственно, 64. " \
-        "Для распознавания комбинации используется четверичная система, а пример кода " \
-        "для получения комбинации по значению от Bot API можно увидеть " \
-        "<a href='https://gist.github.com/MasterGroosha/963c0a82df348419788065ab229094ac'>здесь</a>.\n\n" \
-        "Исходный код бота доступен на <a href='https://github.com/MasterGroosha/telegram-casino-bot'>GitHub</a> " \
-        "и на <a href='https://git.groosha.space/shared/telegram-casino-bot'>GitLab</a>."
-    await message.answer(help_text, disable_web_page_preview=True)
+async def cmd_help(message: Message, l10n: FluentLocalization):
+    await message.answer(
+        l10n.format_value("help-text"),
+        disable_web_page_preview=True
+    )
--- bot/handlers/spin.py
+++ bot/handlers/spin.py
@@ -1,13 +1,16 @@
 from asyncio import sleep
-from textwrap import dedent
 
 from aiogram import Router
-from aiogram.filters import Command, Text
+from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+from fluent.runtime import FluentLocalization
 
-from bot.const import START_POINTS, STICKER_FAIL, SPIN_TEXT, THROTTLE_TIME_SPIN
-from bot.dice_check import get_combo_data
+from bot.config_reader import Settings
+from bot.const import THROTTLE_TIME_SPIN
+from bot.dice_check import get_combo_text, get_score_change
+from bot.filters import SpinTextFilter
 from bot.keyboards import get_spin_keyboard
 
 flags = {"throttling_key": "spin"}
@@ -15,33 +18,31 @@ router = Router()
 
 
 @router.message(Command("spin"), flags=flags)
-@router.message(Text(text=SPIN_TEXT), flags=flags)
-async def cmd_spin(message: Message, state: FSMContext):
+@router.message(SpinTextFilter(), flags=flags)
+async def cmd_spin(message: Message, state: FSMContext, l10n: FluentLocalization, config: Settings):
+    # Get current score
     user_data = await state.get_data()
-    user_score = user_data.get("score", START_POINTS)
+    user_score = user_data.get("score", config.starting_points)
 
     if user_score == 0:
-        await message.answer_sticker(sticker=STICKER_FAIL)
-        await message.answer(
-            "Ваш баланс равен нулю. Вы можете смириться с судьбой и продолжить жить своей жизнью, "
-            "а можете нажать /start, чтобы начать всё заново. Или /stop, чтобы просто убрать клавиатуру."
-        )
+        # todo fix
+        # await message.answer_sticker(sticker=STICKER_FAIL)
+        await message.answer(l10n.format_value("zero-balance"))
         return
 
-    answer_text_template = """\
-        Ваша комбинация: {combo_text} (№{dice_value}).
-        {win_or_lose_text} Ваш счёт: <b>{new_score}</b>.
-        """
-
     # Отправка дайса пользователю
-    msg = await message.answer_dice(emoji="🎰", reply_markup=get_spin_keyboard())
+    msg = await message.answer_dice(emoji=DiceEmoji.SLOT_MACHINE, reply_markup=get_spin_keyboard(l10n))
 
-    # Получение информации о дайсе
-    score_change, combo_text = get_combo_data(msg.dice.value)
+    localized_combo_text = [
+        l10n.format_value(item) for item in get_combo_text(msg.dice.value)
+    ]
+
+    score_change = get_score_change(msg.dice.value)
+
     if score_change < 0:
-        win_or_lose_text = "К сожалению, вы не выиграли."
+        win_or_lose_text = l10n.format_value("spin-fail")
     else:
-        win_or_lose_text = f"Вы выиграли {score_change} очков!"
+        win_or_lose_text = l10n.format_value("spin-success", {"score_change": score_change})
 
     # Обновление счёта
     new_score = user_score + score_change
@@ -49,10 +50,13 @@ async def cmd_spin(message: Message, state: FSMContext):
 
     await sleep(THROTTLE_TIME_SPIN)
     await msg.reply(
-        dedent(answer_text_template).format(
-            combo_text=combo_text,
-            dice_value=msg.dice.value,
-            win_or_lose_text=win_or_lose_text,
-            new_score=new_score
+        l10n.format_value(
+            "after-spin",
+            {
+                "combo_text": ", ".join(localized_combo_text),
+                "dice_value": msg.dice.value,
+                "result_text": win_or_lose_text,
+                "new_score": new_score
+            }
         )
     )
