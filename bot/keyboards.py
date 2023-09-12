a8354fbc8de000bedb4e0befc55534bd08ec62ed
Groosha <groosha@protonmail.com>
2023-09-13 00:36:38 +0800

    Cache created keyboard, since it's always the same until restart

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,7 +1,10 @@
+from functools import cache
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from fluent.runtime import FluentLocalization
 
 
+@cache
 def get_spin_keyboard(l10n: FluentLocalization):
     keyboard = [
         [KeyboardButton(text=l10n.format_value("spin-button-text"))]
